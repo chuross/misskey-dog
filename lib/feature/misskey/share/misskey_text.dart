0fa3f14d81412be58e39c5c78d438de77f52542c
chuross <azarasi.mo.mo@gmail.com>
2023-11-26 15:28:02 +0900

    Update build configuration and separate inline spans

--- lib/feature/misskey/share/misskey_text.dart
+++ lib/feature/misskey/share/misskey_text.dart
@@ -1,62 +1,72 @@
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:misskey_dog/core/extension/async_value.dart';
 import 'package:misskey_dog/feature/emoji/share/misskey_emoji.dart';
 import 'package:misskey_dog/model/emoji/emoji.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-final class MisskeyText extends ConsumerWidget {
-  static final RegExp _emojiReget = RegExp(r':([a-z_]+):');
+part 'misskey_text.g.dart';
 
-  final String text;
-  final TextStyle baseTextStyle;
-  late final List<InlineSpan> inlineSpans;
+final RegExp _emojiReget = RegExp(r':([a-z_]+):');
 
-  MisskeyText({super.key, required this.text, required this.baseTextStyle}) {
-    inlineSpans = _separateSpans(text: text, height: baseTextStyle.fontSize ?? 0);
-  }
+@riverpod
+Future<List<InlineSpan>> _separateInlineSpans(
+  _SeparateInlineSpansRef ref, {
+  required String text,
+  required double height,
+}) async {
+  final emojiMatches = _emojiReget.allMatches(text);
 
-  static List<InlineSpan> _separateSpans({required String text, required height}) {
-    final emojiMatches = _emojiReget.allMatches(text);
+  var spans = <InlineSpan>[];
 
-    var spans = <InlineSpan>[];
-
-    final remnants = text.runes.foldIndexed('', (index, previousValue, rune) {
-      final match = emojiMatches.firstWhereOrNull((element) {
-        return element.start <= index && index <= element.end;
-      });
-
-      // 絵文字にマッチしている間は無視
-      if (match != null && match.start != index) {
-        return '';
-      }
-
-      // 絵文字にマッチしたらそれまでの文字列をTextSpanに変換して、絵文字Spanを追加する
-      if (match?.start == index) {
-        final emoji = LocalEmoji(name: match?.group(1) ?? '');
-        spans.add(TextSpan(text: previousValue));
-        spans.add(WidgetSpan(child: MisskeyEmoji(emoji: emoji, height: height)));
-        return '';
-      }
-
-      // マッチしたものがなければ文字を積んで次のループへ
-      return previousValue + String.fromCharCode(rune);
+  final remnants = text.runes.foldIndexed('', (index, previousValue, rune) {
+    final match = emojiMatches.firstWhereOrNull((element) {
+      return element.start <= index && index <= element.end;
     });
 
-    // 余った文字列があればTextSpanに変換して追加する
-    if (remnants.isNotEmpty) {
-      spans.add(TextSpan(text: remnants));
+    // 絵文字にマッチしている間は無視
+    if (match != null && match.start != index) {
+      return '';
     }
 
-    return spans;
+    // 絵文字にマッチしたらそれまでの文字列をTextSpanに変換して、絵文字Spanを追加する
+    if (match?.start == index) {
+      final emoji = LocalEmoji(name: match?.group(1) ?? '');
+      if (previousValue.isNotEmpty) spans.add(TextSpan(text: previousValue));
+      spans.add(WidgetSpan(child: MisskeyEmoji(emoji: emoji, height: height)));
+      return '';
+    }
+
+    // マッチしたものがなければ文字を積んで次のループへ
+    return previousValue + String.fromCharCode(rune);
+  });
+
+  // 余った文字列があればTextSpanに変換して追加する
+  if (remnants.isNotEmpty) {
+    spans.add(TextSpan(text: remnants));
   }
+
+  return spans;
+}
+
+final class MisskeyText extends ConsumerWidget {
+  final String text;
+  final TextStyle baseTextStyle;
+
+  const MisskeyText({super.key, required this.text, required this.baseTextStyle});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return RichText(
-      softWrap: true,
-      text: TextSpan(
-        style: baseTextStyle,
-        children: inlineSpans,
+    final inlineSpans = ref.watch(_separateInlineSpansProvider(text: text, height: baseTextStyle.fontSize ?? 14));
+
+    return inlineSpans.whenPartialLoading(
+      data: (inlineSpans) => RichText(
+        softWrap: true,
+        text: TextSpan(
+          style: baseTextStyle,
+          children: inlineSpans,
+        ),
       ),
     );
   }
