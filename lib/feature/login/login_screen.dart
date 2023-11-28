762aa653f7ee47b13041ff7ea852ec5f00ccab9b
chuross <azarasi.mo.mo@gmail.com>
2023-11-28 23:52:23 +0900

    Refactor LoginScreen widget to remove unused imports and update build method signature

--- lib/feature/login/login_screen.dart
+++ lib/feature/login/login_screen.dart
@@ -1,7 +1,6 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:i18n_extension/default.i18n.dart';
 import 'package:i18n_extension/i18n_widget.dart';
 import 'package:misskey_dog/core/api/misskey_client.dart';
@@ -11,11 +10,11 @@ import 'package:misskey_dog/core/config/config.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 @RoutePage()
-class LoginScreen extends HookConsumerWidget implements AutoRouteWrapper {
+class LoginScreen extends HookWidget implements AutoRouteWrapper {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final hostTextState = useState('');
 
     return Scaffold(
