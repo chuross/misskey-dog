95c846e679f80dff9ec3f5e83503656a02c387db
chuross <azarasi.mo.mo@gmail.com>
2023-11-30 22:50:54 +0900

    Refactor image detail screen to support zooming

--- lib/feature/image/image_detail_screen.dart
+++ lib/feature/image/image_detail_screen.dart
@@ -1,10 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:misskey_dog/core/extension/string.dart';
 import 'package:misskey_dog/core/extension/widget.dart';
 
 @RoutePage()
-final class ImageDetailScreen extends StatelessWidget {
+final class ImageDetailScreen extends HookWidget {
   final String imageUrl;
 
   const ImageDetailScreen({
@@ -14,20 +15,38 @@ final class ImageDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = useMemoized(() => TransformationController());
+    final currentScale = useState(1.0);
+
+    useEffect(() {
+      controller.addListener(() {
+        currentScale.value = controller.value.getMaxScaleOnAxis();
+      });
+
+      return () => controller.dispose();
+    }, const []);
+
     return Stack(
       children: [
         InteractiveViewer(
+          transformationController: controller,
           minScale: 1.0,
           maxScale: 5.0,
           child: Container(
             color: Colors.blueGrey[700],
             child: Hero(
               tag: imageUrl,
-              child: Dismissible(
-                key: imageUrl.toKey(),
-                direction: DismissDirection.vertical,
-                onDismissed: (_) => context.router.pop(),
-                child: Image.network(imageUrl),
+              child: GestureDetector(
+                onDoubleTap: () => controller.value = Matrix4.identity(),
+                child: AbsorbPointer(
+                  absorbing: currentScale.value != 1.0,
+                  child: Dismissible(
+                    key: imageUrl.toKey(),
+                    direction: DismissDirection.vertical,
+                    onDismissed: (_) => context.router.pop(),
+                    child: Image.network(imageUrl),
+                  ),
+                ),
               ),
             ).align(Alignment.center),
           ),
