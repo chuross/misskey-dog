c58acaf5b9ab25354b50017d6379acd8f2487152
chuross <azarasi.mo.mo@gmail.com>
2023-12-02 20:40:17 +0900

    Refactor home media timeline widget

--- lib/feature/home/home_media_timeline.dart
+++ lib/feature/home/home_media_timeline.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:misskey_dog/core/extension/async_value.dart';
 import 'package:misskey_dog/core/extension/build_context.dart';
+import 'package:misskey_dog/core/extension/string.dart';
+import 'package:misskey_dog/core/extension/widget.dart';
+import 'package:misskey_dog/core/hook/use_load_more.dart';
 import 'package:misskey_dog/core/logger/logger_provider.dart';
 import 'package:misskey_dog/feature/note/share/note_item.dart';
 import 'package:misskey_dog/model/note/note_provider.dart';
 
-final class HomeMediaTimeline extends ConsumerWidget {
+final class HomeMediaTimeline extends HookConsumerWidget {
   const HomeMediaTimeline({super.key});
 
   @override
@@ -14,34 +17,53 @@ final class HomeMediaTimeline extends ConsumerWidget {
     final provider = localNoteIdsWithCacheProvider(hasFiles: true);
     final noteIds = ref.watch(provider);
 
+    final controller = useLoadMore(onNext: () => ref.watch(provider.notifier).fetchNext());
+
     return noteIds.whenScreenLoading(
       log: ref.watch(logProvider),
       onRetry: () => ref.invalidate(provider),
       data: (notesIds) {
         return RefreshIndicator(
           onRefresh: () async => ref.invalidate(provider),
-          child: ListView.builder(
-            padding: const EdgeInsets.only(bottom: 80),
-            itemCount: notesIds.length,
-            itemBuilder: (context, index) {
-              final note = ref.watch(cachedNoteProvider(id: notesIds[index]));
-              if (note == null) return const SizedBox.shrink();
+          child: CustomScrollView(
+            controller: controller,
+            slivers: [
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    final note = ref.watch(cachedNoteProvider(id: notesIds[index]));
+                    if (note == null) return const SizedBox.shrink();
 
-              return Container(
-                decoration: BoxDecoration(
-                  border: Border(
-                    bottom: Divider.createBorderSide(context, color: context.dividerColorWithOpacity30),
-                  ),
-                ),
-                child: NoteItem(
-                  key: ValueKey(note.id),
-                  note: note,
-                  onReactionTap: (emoji) {
-                    ref.read(cachedNoteProvider(id: note.id).notifier).reaction(emoji);
+                    return Container(
+                      decoration: BoxDecoration(
+                        border: Border(
+                          bottom: Divider.createBorderSide(context, color: context.dividerColorWithOpacity30),
+                        ),
+                      ),
+                      child: NoteItem(
+                        key: note.id.toKey(),
+                        note: note,
+                        onReactionTap: (emoji) {
+                          ref.read(cachedNoteProvider(id: note.id).notifier).reaction(emoji);
+                        },
+                      ),
+                    );
                   },
+                  childCount: notesIds.length,
                 ),
-              );
-            },
+              ),
+              SliverToBoxAdapter(
+                child: noteIds
+                    .whenPartialLoading(
+                        data: (_) => const SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: CircularProgressIndicator(strokeWidth: 2),
+                            ))
+                    .align(Alignment.center)
+                    .padding(const EdgeInsets.all(32)),
+              ),
+            ],
           ),
         );
       },
