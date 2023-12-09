c2a61135d172f7abf55ff6a818205c60f223e808
chuross <azarasi.mo.mo@gmail.com>
2023-12-09 17:27:04 +0900

    Add CachedNetworkImage to display image

--- lib/feature/image/image_detail_screen.dart
+++ lib/feature/image/image_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:misskey_dog/core/extension/string.dart';
@@ -55,7 +56,7 @@ final class ImageDetailScreen extends HookWidget {
                     resizeDuration: null,
                     direction: DismissDirection.vertical,
                     onDismissed: (_) => context.router.pop(),
-                    child: Image.network(imageUrl),
+                    child: CachedNetworkImage(imageUrl: imageUrl),
                   ),
                 ),
               ),
