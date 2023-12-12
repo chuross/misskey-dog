18db88e1a5397e11bf65df7a09257cd3a0f80f0c
chuross <azarasi.mo.mo@gmail.com>
2023-12-12 22:24:44 +0900

    Refactor inline span separation logic

--- lib/feature/misskey/share/misskey_text.dart
+++ lib/feature/misskey/share/misskey_text.dart
@@ -11,6 +11,12 @@ final RegExp _emojiRegex = RegExp(r':([A-Za-z0-9_]+):');
 final RegExp _hashTagRegex = RegExp(r'(?<=\s)#\S+');
 final RegExp _urlRegex = RegExp(r'https?://([\w-]+\.)+[\w-]+(/[\w-./?%&=#]*)?');
 
+enum _MatchKind {
+  emoji,
+  hashTag,
+  url,
+}
+
 List<InlineSpan> _separateInlineSpans({
   required BuildContext context,
   required String text,
@@ -24,129 +30,71 @@ List<InlineSpan> _separateInlineSpans({
     return '_';
   });
 
-  final emojiMatches = _emojiRegex.allMatches(replacedText);
-  final hashTagMatches = _hashTagRegex.allMatches(replacedText);
-  final urlMatches = _urlRegex.allMatches(replacedText);
+  final emojiMatches = _emojiRegex.allMatches(replacedText).map((e) => (_MatchKind.emoji, e));
+  final hashTagMatches = _hashTagRegex.allMatches(replacedText).map((e) => (_MatchKind.hashTag, e));
+  final urlMatches = _urlRegex.allMatches(replacedText).map((e) => (_MatchKind.url, e));
 
-  var spans = <InlineSpan>[];
+  final matches = [
+    ...emojiMatches,
+    ...hashTagMatches,
+    ...urlMatches,
+  ].sortedBy<num>((e) => e.$2.start);
 
-  final remnants = text.characters.foldIndexed('', (index, previousValue, char) {
-    if ([
-      _applyEmojiSpan(
-        spans: spans,
-        previousValue: previousValue,
-        index: index,
-        height: height,
-        emojiMatches: emojiMatches,
-        externalTextEmojiUrlMap: externalTextEmojiUrlMap,
-      ),
-      _applyCliclableTextSpan(
-        spans: spans,
-        previousValue: previousValue,
-        index: index,
-        matches: hashTagMatches,
-        context: context,
-        onPressed: onHashTagPressed,
-      ),
-      _applyCliclableTextSpan(
-        spans: spans,
-        previousValue: previousValue,
-        index: index,
-        matches: urlMatches,
-        context: context,
-        onPressed: onUrlPressed,
-      ),
-    ].any((element) => element)) {
-      return '';
-    } else {
-      // マッチしたものがなければ文字を積んで次のループへ
-      return previousValue + char;
-    }
-  });
-
-  // 余った文字列があればTextSpanに変換して追加する
-  if (remnants.isNotEmpty) {
-    spans.add(TextSpan(text: remnants));
+  if (matches.isEmpty) {
+    return [TextSpan(text: text)];
   }
+
+  final textChars = text.characters;
+
+  final spans = matches.foldIndexed(<InlineSpan>[], (index, spans, curr) {
+    final prevMatch = index > 0 ? matches.elementAtOrNull(index - 1) : null;
+    final plainTextSpan = TextSpan(text: textChars.getRange(prevMatch?.$2.end ?? 0, curr.$2.start).join());
+    final additionalSpan = switch (curr.$1) {
+      _MatchKind.emoji => _resolveEmoji(match: curr.$2, height: height, externalTextEmojiUrlMap: externalTextEmojiUrlMap),
+      _MatchKind.hashTag => _resolveCliclableTextSpan(match: curr.$2, height: height, context: context, onPressed: onHashTagPressed),
+      _MatchKind.url => _resolveCliclableTextSpan(match: curr.$2, height: height, context: context, onPressed: onUrlPressed),
+    };
+
+    return [...spans, plainTextSpan, additionalSpan];
+  });
 
   return spans;
 }
 
-bool _applyEmojiSpan({
-  required List<InlineSpan> spans,
-  required String previousValue,
-  required int index,
+WidgetSpan _resolveEmoji({
+  required RegExpMatch match,
   required double height,
-  required Iterable<RegExpMatch> emojiMatches,
   required Map<String, String> externalTextEmojiUrlMap,
 }) {
-  final match = emojiMatches.firstWhereOrNull((element) {
-    return element.start <= index && index < element.end;
-  });
+  final emojiName = match.group(1);
+  final url = externalTextEmojiUrlMap[emojiName];
+  final host = url?.isNotEmpty == true ? 'unknown' : '.';
 
-  // 絵文字にマッチしている間は無視
-  if (match != null && match.start != index) {
-    return true;
-  }
+  final emoji = switch (host) {
+    (String host) when host == '.' => LocalEmoji(name: emojiName ?? ''),
+    _ => ExternalEmoji(name: emojiName ?? '', host: host, url: url!),
+  };
 
-  // 絵文字にマッチしたらそれまでの文字列をTextSpanに変換して、絵文字Spanを追加する
-  if (match?.start == index) {
-    final emojiName = match?.group(1);
-    final url = externalTextEmojiUrlMap[emojiName];
-    final host = url?.isNotEmpty == true ? 'unknown' : '.';
-    final emoji = switch (host) {
-      (String host) when host == '.' => LocalEmoji(name: emojiName ?? ''),
-      _ => ExternalEmoji(name: emojiName ?? '', host: host, url: url!),
-    };
-
-    if (previousValue.isNotEmpty) spans.add(TextSpan(text: previousValue));
-
-    spans.add(WidgetSpan(
-      child: EmojiView(emoji: emoji, height: height + 4),
-      alignment: PlaceholderAlignment.middle,
-      baseline: TextBaseline.alphabetic,
-    ));
-
-    return true;
-  }
-
-  return false;
+  return WidgetSpan(
+    child: EmojiView(emoji: emoji, height: height + 4),
+    alignment: PlaceholderAlignment.middle,
+    baseline: TextBaseline.alphabetic,
+  );
 }
 
-bool _applyCliclableTextSpan({
-  required List<InlineSpan> spans,
-  required String previousValue,
-  required int index,
-  required Iterable<RegExpMatch> matches,
+TextSpan _resolveCliclableTextSpan({
+  required RegExpMatch match,
+  required double height,
   required BuildContext context,
   required Function(String) onPressed,
-  int targetMatchGroup = 0,
 }) {
-  final match = matches.firstWhereOrNull((element) {
-    return element.start <= index && index < element.end;
-  });
+  final text = match.group(0);
 
-  // URLにマッチしている間は無視
-  if (match != null && match.start != index) {
-    return true;
-  }
-
-  // URLにマッチしたらそれまでの文字列をTextSpanに変換して、絵文字Spanを追加する
-  if (match?.start == index) {
-    if (previousValue.isNotEmpty) spans.add(TextSpan(text: previousValue));
-
-    final text = match?.group(targetMatchGroup);
-
-    spans.add(TextSpan(
-      text: text,
-      style: TextStyle(color: context.theme.primaryColor),
-      recognizer: TapGestureRecognizer()..onTap = () => onPressed(text ?? ''),
-    ));
-
-    return true;
-  }
-
-  return false;
+  return TextSpan(
+    text: text,
+    style: TextStyle(color: context.theme.primaryColor, fontSize: height),
+    recognizer: TapGestureRecognizer()..onTap = () => onPressed(text ?? ''),
+  );
 }
 
 final class MisskeyText extends HookWidget {
