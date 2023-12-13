9aef8bb9820886cc16622dceef79ebd8d2b946c8
chuross <azarasi.mo.mo@gmail.com>
2023-12-13 13:15:27 +0900

    Refactor notification item widget

--- lib/feature/notification/share/notification_item.dart
+++ lib/feature/notification/share/notification_item.dart
@@ -1,11 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:i18n_extension/default.i18n.dart';
 import 'package:misskey_dog/core/extension/build_context.dart';
 import 'package:misskey_dog/core/extension/date_time.dart';
 import 'package:misskey_dog/core/extension/object.dart';
 import 'package:misskey_dog/core/extension/widget.dart';
 import 'package:misskey_dog/feature/misskey/share/misskey_text.dart';
+import 'package:misskey_dog/model/emoji/emoji_provider.dart';
 import 'package:misskey_dog/model/notification/notification.dart' as model;
 import 'package:misskey_dog/model/notification/notification_type.dart';
 
@@ -17,14 +19,7 @@ final class NotificationItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return switch (notification.kind) {
-      NotificationKind.reaction => _item(
-          context: context,
-          imageUrl: notification.user?.avatarUrl ?? '',
-          text: '%sがリアクション'.fill([notification.user?.displayName ?? '']).i18n,
-          subText: notification.note?.text ?? notification.note?.cw,
-          createdAt: notification.createdAt,
-          externalEmojiUrlMap: notification.user?.externalEmojiUrlMap ?? const {},
-        ),
+      NotificationKind.reaction => _ReactionNotificationItem(notification: notification),
       NotificationKind.renote => _item(
           context: context,
           imageUrl: notification.user?.avatarUrl ?? '',
@@ -43,51 +38,92 @@ final class NotificationItem extends StatelessWidget {
       _ => const SizedBox.shrink(),
     };
   }
+}
 
-  Widget _item({
-    required BuildContext context,
-    required String imageUrl,
-    String? subImageUrl,
-    required String text,
-    String? subText,
-    required DateTime createdAt,
-    Map<String, String> externalEmojiUrlMap = const {},
-    Map<String, String> subExternalEmojiUrlMap = const {},
-  }) {
-    return Row(
-      children: [
-        SizedBox.square(
-          dimension: 48,
-          child: CircleAvatar(
-            foregroundImage: CachedNetworkImageProvider(imageUrl),
-            child: subImageUrl?.map(
-              (p) => CircleAvatar(
-                foregroundImage: CachedNetworkImageProvider(subImageUrl),
-              ),
+final class _ReactionNotificationItem extends ConsumerWidget {
+  final model.Notification notification;
+
+  const _ReactionNotificationItem({required this.notification});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final emoji = ref.watch(localEmojiProvider(emojiName: notification.reaction?.map((p) => p.substring(1, p.length - 1)) ?? ''));
+
+    switch (emoji) {
+      case AsyncData(:final value):
+        return _item(
+          context: context,
+          imageUrl: notification.user?.avatarUrl ?? '',
+          subImageUrl: value.url,
+          text: '%sがリアクション'.fill([notification.user?.displayName ?? '']).i18n,
+          subText: notification.note?.text ?? notification.note?.cw,
+          createdAt: notification.createdAt,
+          externalEmojiUrlMap: notification.user?.externalEmojiUrlMap ?? const {},
+        );
+      default:
+        return const SizedBox.shrink();
+    }
+  }
+}
+
+Widget _item({
+  required BuildContext context,
+  required String imageUrl,
+  String? subImageUrl,
+  required String text,
+  String? subText,
+  required DateTime createdAt,
+  Map<String, String> externalEmojiUrlMap = const {},
+  Map<String, String> subExternalEmojiUrlMap = const {},
+}) {
+  return Row(
+    children: [
+      Stack(
+        clipBehavior: Clip.none,
+        children: [
+          SizedBox.square(
+            dimension: 48,
+            child: CircleAvatar(
+              foregroundImage: CachedNetworkImageProvider(imageUrl),
             ),
           ),
-        ),
-        const SizedBox(width: 8),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            MisskeyText(
-              text: text,
-              baseTextStyle: context.textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
-              externalTextEmojiUrlMap: externalEmojiUrlMap,
+          subImageUrl.mapOrElse(
+            (subImageUrl) => Positioned(
+              bottom: -8,
+              right: -8,
+              child: Container(
+                padding: const EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+                child: CachedNetworkImage(imageUrl: subImageUrl, width: 24, height: 24),
+              ),
             ),
-            SizedBox(height: subText?.map((_) => 8) ?? 0),
-            subText.mapOrElse((text) {
-              return MisskeyText(
-                  text: text,
-                  baseTextStyle: context.textTheme.bodySmall?.copyWith(color: Colors.grey.shade700),
-                  externalTextEmojiUrlMap: subExternalEmojiUrlMap);
-            }, elseValue: const SizedBox.shrink())
-          ],
-        ).expanded(),
-        const SizedBox(width: 24),
-        Text(createdAt.elapsedTimeLabel, style: context.textTheme.labelSmall),
-      ],
-    ).padding(const EdgeInsets.all(16));
-  }
+            elseValue: const SizedBox.shrink(),
+          ),
+        ],
+      ),
+      const SizedBox(width: 16),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          MisskeyText(
+            text: text,
+            baseTextStyle: context.textTheme.bodyMedium?.copyWith(fontWeight: FontWeight.bold),
+            externalTextEmojiUrlMap: externalEmojiUrlMap,
+          ),
+          SizedBox(height: subText?.map((_) => 8) ?? 0),
+          subText.mapOrElse((text) {
+            return MisskeyText(
+                text: text,
+                baseTextStyle: context.textTheme.bodySmall?.copyWith(color: Colors.grey.shade700),
+                externalTextEmojiUrlMap: subExternalEmojiUrlMap);
+          }, elseValue: const SizedBox.shrink())
+        ],
+      ).expanded(),
+      const SizedBox(width: 24),
+      Text(createdAt.elapsedTimeLabel, style: context.textTheme.labelSmall),
+    ],
+  ).padding(const EdgeInsets.all(16));
 }
