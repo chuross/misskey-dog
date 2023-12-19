c482d3dd62f0c55fa0cdf3f1f45d626ad0506189
chuross <azarasi.mo.mo@gmail.com>
2023-12-19 18:17:14 +0900

    Refactor oauthCallbackUrl in Config class

--- lib/core/config/config.dart
+++ lib/core/config/config.dart
@@ -1,7 +1,6 @@
 import 'package:universal_platform/universal_platform.dart';
 
 final class Config {
-  static final oauthCallbackUrl = UniversalPlatform.isWeb
-      ? '${Uri.base.origin}/${Uri.base.pathSegments.join('/')}#/auth/auth_callback'
-      : 'misskeydog://auth/auth_callback';
+  static final oauthCallbackUrl =
+      UniversalPlatform.isWeb ? '${Uri.base.origin}/${Uri.base.pathSegments.join('/')}/auth_callback' : 'misskeydog://auth/auth_callback';
 }
