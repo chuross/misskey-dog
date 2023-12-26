e3bad91be1084998465bc3ac86737daa77995830
chuross <azarasi.mo.mo@gmail.com>
2023-12-26 09:57:31 +0900

    Add imports and update UI in account_screen.dart

--- lib/feature/account/account_screen.dart
+++ lib/feature/account/account_screen.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:i18n_extension/default.i18n.dart';
+import 'package:misskey_dog/core/extension/build_context.dart';
+import 'package:misskey_dog/core/extension/widget.dart';
+import 'package:misskey_dog/feature/home/home_screen.dart';
 import 'package:misskey_dog/feature/login/login_screen.dart';
+import 'package:misskey_dog/feature/user/user_detail/user_detail_screen.dart';
 import 'package:misskey_dog/model/account/account_provider.dart';
 
 final class AccountRoute extends GoRouteData {
@@ -28,12 +33,20 @@ final class AccountScreen extends ConsumerWidget {
       appBar: AppBar(title: Text('アカウント'.i18n)),
       body: ListView(
         children: [
-          OutlinedButton(
-            onPressed: () async {
-              await ref.read(accountStateProvider.notifier).clear();
+          ListTile(
+            onTap: () {
+              ref.read(accountStateProvider.future).then((value) {
+                UserDetailRoute(userId: value?.user.id ?? '').push(context);
+              });
             },
-            child: Text('ログアウト'.i18n),
-          )
+            title: Text('プロフィール'.i18n),
+          ),
+          Divider(color: context.dividerColorWithOpacity30),
+          ListTile(
+            onTap: () async => await ref.read(accountStateProvider.notifier).clear(),
+            title: Text('ログアウト'.i18n).align(Alignment.center),
+            textColor: CupertinoColors.destructiveRed,
+          ),
         ],
       ),
     );
